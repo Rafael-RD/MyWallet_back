215e52df7ee30e0b90056a480ca9142ae963031f
Rafael-RD <rafaelreinertd@hotmail.com>
2023-09-26 14:45:29 -0300

    refac: changed default port

--- src/app.js
+++ src/app.js
@@ -9,4 +9,5 @@ app.use(express.json());
 app.use(cors());
 app.use(indexRouter);
 
-app.listen(process.env.PORT || 3000, () => console.log(`Server on ${process.env.PORT || 3000}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
